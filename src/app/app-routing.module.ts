5307560394a7ac1ad97f301a9fbaaf6f6c02641f
Carito-Gutierrez <sankrito0417@gmail.com>
2022-03-09 10:49:33 -0500

    Agrega componentes client, comments, login, page404, payment, payment-methods y register; ademas agrega rutas en app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { Page404Component } from './components/page404/page404.component';
+import { PaymentComponent } from './components/payment/payment.component';
+import { RegisterComponent } from './components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'login', component: LoginComponent},
+{path:'register', component: RegisterComponent},
+{path:'payment', component: PaymentComponent},
+
+{ path: '', redirectTo: '/home', pathMatch: 'full'},
+{ path: '**', component: Page404Component},
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ClientComponent } from './components/client/client.component';
+import { CommentsComponent } from './components/comments/comments.component';
+import { PaymentMethodsComponent } from './components/payment-methods/payment-methods.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { Page404Component } from './components/page404/page404.component';
+import { PaymentComponent } from './components/payment/payment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    ClientComponent,
+    CommentsComponent,
+    PaymentMethodsComponent,
+    LoginComponent,
+    RegisterComponent,
+    Page404Component,
+    PaymentComponent
   ],
   imports: [
     BrowserModule,
