7c653a63af97637d95c3e0dc73a829a77c45f35f
Carito-Gutierrez <sankrito0417@gmail.com>
2022-03-09 16:38:46 -0500

    Agrega html y css de componente payment-methods, incluyendo imagenes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { Page404Component } from './components/page404/page404.component';
+import { PaymentMethodsComponent } from './components/payment-methods/payment-methods.component';
 import { PaymentComponent } from './components/payment/payment.component';
 import { RegisterComponent } from './components/register/register.component';
 
@@ -9,6 +10,7 @@ const routes: Routes = [
 {path:'login', component: LoginComponent},
 {path:'register', component: RegisterComponent},
 {path:'payment', component: PaymentComponent},
+{path:'payment-methods', component: PaymentMethodsComponent},
 
 { path: '', redirectTo: '/home', pathMatch: 'full'},
 { path: '**', component: Page404Component},
