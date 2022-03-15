2e6394ab6695500521278ddb8210c8dbfa752c3d
DarwingSanchez <darwing.sanchez@hotmail.com>
2022-03-15 16:43:13 -0500

    [uploading] uploading to merge

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UploadImgService } from 'src/app/services/uploadImage/upload-img.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  public imgRta = '';
+
+  constructor(
+    public uploadImageService : UploadImgService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  onUpload(event: Event){
+    console.log('Ejecuto el botón onUpload')
+    const element = event.target as HTMLInputElement;
+    const file = element.files?.item(0)
+    if(file){
+      this.uploadImageService.upload(file)
+      .subscribe({
+        next: data => {
+          console.log(data)
+        },
+        error: err => {
+          console.log(err)
+        }
+      })
+    }
+  }
 }
